2e2d66052d367664a31f0bb72109364f20d6f038
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-01-24 09:00:31 +0100

    Building main page and navbar

--- view/main.php
+++ view/main.php
@@ -4,15 +4,59 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/main.css">
     <title>Főoldal</title>
 </head>
 <body>
-    <h1>Hello World Lorem Ipsum</h1>
-    <img src="../assets/images/floorplan.png" #usemap="emelet">
-    <!--<map name="emelet">-->
-    <!--    <area>-->
-    <!--</map>-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<!--    <h1 class="">Hello World Lorem Ipsum</h1>-->
+
+<div class="container">
+    <!-- Floor map -->
+    <section>
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>101</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>102</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>103</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>104</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-3 room-holder"></div>
+                    <div class="col-12 col-md-3 room-holder"></div>
+                    <div class="col-12 col-md-3 room-holder"></div>
+                    <div class="col-12 col-md-3 room-holder"></div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>105</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>106</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-tile justify-content-center align-items-center">
+                        <span>107</span>
+                    </div>
+                    <div class="col-12 col-md-3 room-holder"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <img src="../assets/images/floorplan.png" #usemap="emelet" class="layout mx-auto d-block">
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
 </body>
