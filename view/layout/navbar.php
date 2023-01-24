3196f299eaf10867a53c789e37feda2e9b878351
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-01-24 09:24:00 +0100

    Navbar styling and css

--- view/layout/navbar.php
+++ view/layout/navbar.php
@@ -1,1 +1,33 @@
-<?php
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link rel="stylesheet" href="../../assets/css/navbar.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="fa-regular fa-book-open-reader"></i>
+                    Reservations
+                </a>
+
+            </li>
+            <li>
+                <a href="">
+                    About
+                </a>
+            </li>
+            <li>
+                <!--Login/Register-->
+            </li>
+        </ul>
+    </nav>
+    <script src="../../assets/js/fontawesome.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+			crossorigin="anonymous"></script>
+</body>
