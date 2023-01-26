2d0e0f559fbec82c688150d02d519d617fc1c617
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-01-26 09:29:41 +0100

    Navbar with css and js.

--- view/layout/navbar.php
+++ view/layout/navbar.php
@@ -1,46 +1,30 @@
 <head>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-		  integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-	<link rel="stylesheet" href="../../assets/css/navbar.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../assets/css/navbar.css">
 </head>
 <body>
-    <nav class="menu">
-        <ul>
-            <li>
-                <a href="#">
-                    <i class="fa-regular fa-book-open-reader"></i>
-                    Reservations
-                </a>
 
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa-regular fa-circle-question"></i>
-                    About
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa-regular fa-envelope"></i>
-                    Email us!
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <i class="fa-regular fa-star-sharp"></i>
-                    Write a review!
-                </a>
-            </li>
-            <li>
-                <!--Login/Register-->
-            </li>
-        </ul>
-    </nav>
+        <div class="sidebar">
+            <button>M</button>
+        </div>
+
+
     <script src="../../assets/js/fontawesome.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-			integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-			crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+            crossorigin="anonymous">
+
+    </script>
+    <script>
+        const sidebar = document.querySelector('.sidebar');
+        const mainContent = document.querySelector('.main-content');
+        document.querySelector('button').onclick = function() {
+            sidebar.classList.toggle('sidebar_small');
+            mainContent.classList.toggle('main-content_larger');
+        }
+    </script>
 </body>
