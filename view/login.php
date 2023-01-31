dd4b1dc5c7be5130b500582dcbc16edf3f2ea434
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-01-31 08:23:47 +0100

    I did nothing.

--- view/login.php
+++ view/login.php
@@ -8,20 +8,22 @@
 	<link rel="stylesheet" href="../assets/css/login.css">
 </head>
 <body>
-	<?php
-		if(!isset($_SESSION["nev"])) {
-			echo '<form action="" method="POST">
-					Felhasználónév: <input type="text" name="felhnev" placeholder="pl.:kiskarcsi123" required><br>
-					Jelszó: <input type="password" name="jelszo" required><br>
-					<input type="submit" value="Belépés" class="buttons">
-				</form>';
-		} else {
-			echo '<form action="" method="post" enctype="multipart/form-data" style="text-align: center;">
-					Válaszd ki a profilképet amit fel szeretnél tölteni:<br>
-					<input type="file" name="profilkep" id="profilkep">
-					<br><input type="submit" class="buttons" value="Upload Image" name="submit">
-				</form>';
-		}
-	?>
+    <div class="panel">
+	    <?php
+	    	if(!isset($_SESSION[""])) {
+	    		echo '<form action="" method="POST">
+	    				Felhasználónév: <input type="text" name="felhnev" placeholder="pl.:kiskarcsi123" required><br>
+	    				Jelszó: <input type="password" name="jelszo" required><br>
+	    				<input type="submit" value="Belépés" class="buttons">
+	    			</form>';
+	    	} else {
+	    		echo '<form action="" method="post" enctype="multipart/form-data" style="text-align: center;">
+	    				Válaszd ki a profilképet amit fel szeretnél tölteni:<br>
+	    				<input type="file" name="profilkep" id="profilkep">
+	    				<br><input type="submit" class="buttons" value="Upload Image" name="submit">
+	    			</form>';
+	    	}
+	    ?>
+    </div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
