f800bd25831d6feaa6cc6f5bd304ac8c66c2fa52
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-02-21 10:39:13 +0100

    Sidebar changes

--- View/layout/sidebar.php
+++ View/layout/sidebar.php
@@ -1,9 +1,31 @@
 <div class="d-flex flex-column flex-shrink-0 bg-light sidebar">
-    <a href="/korondi/home" class="d-block p-3 link-dark text-decoration-none" title data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
+    <a href="/korondi/home" class="d-block p-3 link-dark text-decoration-none" title="Főoldal" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
         <image class="bi" width="40" height="32" href="Assets/images/HotelLogo.png" title="Logo" alt="Logo"></image>
         <span class="visually-hidden">Főoldal</span>
     </a>
-    <ul class="">
+    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+        <li class="nav-item">
+            <a href="/korondi/home" class="nav-link active py-3 border-bottom" title="Főoldal" data-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
+                <i class="fa-regular fa-house"></i>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link py-3 border-bottom" >
 
+            </a>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <div class="dropdown border-top">
+            <ul class="dropdown-menu text-small">
+
+            </ul>
+        </div>
     </ul>
 </div>
+<script>
+    $(document).ready(function() {
+        $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+    });
+</script>
