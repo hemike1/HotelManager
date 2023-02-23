bed77b6d939aa4329bc61e44c79470d8ddac2f43
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-02-23 13:24:54 +0100

    Finishing touches for the day

--- View/layout/sidebar.php
+++ View/layout/sidebar.php
@@ -1,48 +1,48 @@
-<div class="d-flex flex-column flex-shrink-0 bg-light sidebar">
-    <a href="/korondi/home" class="d-block p-3 link-dark text-decoration-none border-bottom border-5" title="Főoldal" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
-        <image class="bi" width="38" height="40" src="Assets/images/HotelLogo.png" title="Logo" alt="Logo"></image>
+<div class="d-flex flex-column flex-shrink-0 bg-light sidebar position-fixed bg-color-custom" style="height: 100vh;">
+    <a href="/korondi/home" class="d-block p-1 link-dark text-decoration-none text-center border-bottom border-5" title="Főoldal" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
+        <image class="bi" width="35" src="Assets/images/HotelLogo.png" title="Logo" alt="Logo"></image>
     </a>
     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
         <li>
             <a href="/korondi/home" class="nav-link py-3 border-bottom" title="Főoldal" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Főoldal">
-                <i class="fa-regular fa-house"></i>
+                <i class="fa-regular fa-house icon-color-custom"></i>
             </a>
         </li>
         <li>
             <a href="/korondi/review" class="nav-link py-3 border-bottom" title="Értékeljen minket!" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Értékeljen minket!">
-                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star icon-color-custom"></i>
             </a>
         </li>
         <li>
             <a href="/korondi/contacts" class="nav-link py-3 border-bottom" title="Elérhetőségek" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Elérhetőségek">
-                <i class="fa-regular fa-circle-phone"></i>
+                <i class="fa-regular fa-circle-phone icon-color-custom"></i>
             </a>
         </li>
         <li>
             <a href="/korondi/info" class="nav-link py-3 border-bottom" title="Akadálymentesítés" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Akadálymentesítés">
-                <i class="fa-regular fa-wheelchair"></i>
+                <i class="fa-regular fa-wheelchair icon-color-custom"></i>
             </a>
         </li>
         <li>
             <a href="/korondi/images" class="nav-link py-3 border-bottom border-5" title="Képeink" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Képeink">
-                <i class="fa-regular fa-image"></i>
+                <i class="fa-regular fa-image icon-color-custom"></i>
             </a>
         </li>
         <?php if($user->getPermission() >= 2): ?>
         <li>
             <a href="/korondi/reservations" class="nav-link py-3 border-bottom" title="Foglalások" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Foglalások">
-                <i class="fa-regular fa-file"></i>
+                <i class="fa-regular fa-file icon-color-custom"></i>
             </a>
         </li>
         <?php endif; if($user->getPermission() == 3): ?>
         <li>
             <a href="/korondi/admin/allreserv" class="nav-link py-3 border-bottom" title="Összes foglalás" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Összes foglalás">
-                <i class="fa-regular fa-file"></i>
+                <i class="fa-regular fa-file icon-color-custom"></i>
             </a>
         </li>
         <li>
             <a href="/korondi/admin/usermgmt" class="nav-link py-3 border-bottom border-5" title="Recepciós felvétel" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Recepciós felvétel">
-                <i class="fa-regular fa-file"></i>
+                <i class="fa-regular fa-file icon-color-custom"></i>
             </a>
         </li>
         <?php endif; ?>
