d63eb4007c56ec6e12f84947a03c2ee0c481fdf0
hemike1 <44871766+hemike1@users.noreply.github.com>
2023-03-01 18:19:56 +0100

    Event listeners on preview screen

--- Assets/js/counters.js
+++ Assets/js/counters.js
@@ -1,9 +1,33 @@
-const desc = document.getElementById('newroomdescription');
-desc.addEventListener('input', () => {
-    if(desc.value.length !== 0) {
-        document.getElementById('newroomcount').innerHTML = "200/" + (200 - desc.value.length);
+const newRoomDesc = document.getElementById('newroomdescription');
+const previewDescription = document.getElementById('prewDesc');
+const inputIconSet = document.getElementById('iconOptions');
+const previewFeatures = document.getElementById('prewFeatures');
+const newRoomPrice = document.getElementById('newRoomPrice');
+const newRoomFloor = document.getElementById('formRoomFloor');
+const newRoomNumber = document.getElementById('formRoomNum');
+
+newRoomDesc.addEventListener('input', () => {
+    if(newRoomDesc.value.length !== 0) {
+        document.getElementById('newroomcount').innerHTML = "200/" + (200 - newRoomDesc.value.length);
     } else {
         document.getElementById('newroomcount').innerHTML = "Leírás, max. 200 karakter!";
     }
-    desc.style.height = "";desc.style.height = desc.scrollHeight + "px";
+    newRoomDesc.style.height = "";newRoomDesc.style.height = newRoomDesc.scrollHeight + "px";
+    previewDescription.textContent = newRoomDesc.value;
 });
+
+newRoomPrice.addEventListener('input', () => {
+    document.getElementById('prewPrice').innerHTML = newRoomPrice.value+".-HUF/<i class=\"fa-regular fa-moon-stars\"></i>";
+});
+
+newRoomFloor.addEventListener('input', () => {
+    document.getElementById('prewRoomNum').innerHTML = "Emelet: "+newRoomFloor.value+" | Szobaszám: "+newRoomNumber.value;
+});
+
+newRoomNumber.addEventListener('input', () => {
+    document.getElementById('prewRoomNum').innerHTML = "Emelet: "+newRoomFloor.value+" | Szobaszám: "+newRoomNumber.value;
+});
+
+inputIconSet.addEventListener("change", () => {
+    previewFeatures.textContent = inputIconSet.value;
+})
--- View/admin/newroom.php
+++ View/admin/newroom.php
@@ -1,58 +1,73 @@
-<div class="col-6 m-5">
+<div class="row">
+    <div class="col-10">
 
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">@</span>
-        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-    </div>
+    <!-- Done stuff -->
+        <div class="input-group mb-3">
+            <label for="formSize" class="input-group-text m-0">Szoba méret</label>
+            <select id="formSize" class="form-select me-1">
+                <option value="" selected>Válassz...</option>
+                <option value="Small">Kicsi</option>
+                <option value="Medium">Közepes</option>
+                <option value="Large">Nagy</option>
+            </select>
+            <input type="number" id="formRoomFloor" class="form-control col-3" placeholder="Csak számok!">
+            <span for="formRoomFloor" class="input-group-text">Emelet</span>
+            <input type="number" id="formRoomNum" class="form-control ms-1 col-3" placeholder="Csak számok!">
+            <span class="input-group-text">Szoba szám</span>
+        </div>
+
+        <!-- half done, TODO: waiting to be implemented -->
+        <div class="input-group mb-3 col-6">
+            <label for="formFile" class="form-label">Bemutató kép a szobáról</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+            <div class="input-group mb-3 col-6">
+                <input type="number" class="form-control ms-1 col-3" placeholder="Csak számok!">
+                <span class="input-group-text">Fő/szoba</span>
+            </div>
 
 
 
+        <!-- end of half done -->
 
-
-<!-- Done stuff -->
-    <div class="input-group mb-3">
-        <label for="formSize" class="input-group-text m-0">Szoba méret</label>
-        <select id="formSize" class="form-select me-1">
-            <option value="" selected>Válassz...</option>
-            <option value="Small">Kicsi</option>
-            <option value="Medium">Közepes</option>
-            <option value="Large">Nagy</option>
-        </select>
-        <input type="number" id="formRoomFloor" class="form-control col-3" placeholder="Csak számok!">
-        <span for="formRoomFloor" class="input-group-text">Emelet</span>
-        <input type="number" class="form-control ms-1 col-3" placeholder="Csak számok!">
-        <span class="input-group-text">Szoba szám</span>
-    </div>
-
-    <!-- half done, TODO: waiting to be implemented -->
-    <div class="mb-3 col-6">
-        <label for="formFile" class="form-label">Bemutató kép a szobáról</label>
-        <input class="form-control" type="file" id="formFile">
-    </div>
-    <!-- end of half done -->
-
-    <div class="input-group mb-3">
-        <label class="input-group-text m-0" for="inputGroupSelect01">Jellemzők</label>
-        <select class="form-select me-1" id="inputGroupSelect01">
-            <option selected>Válassz...</option>
-            <?php
-            $sql = $this->prepare('SELECT * FROM '.$GLOBALS['prefix'].'features');
-            $sql->execute();
-            if($result = $sql->get_result()){
-                if($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo '<option value="'.$row['featureId'].'">'.$row['featureName'].'</option>';
+        <div class="input-group mb-3">
+            <label class="input-group-text m-0" for="inputGroupSelect01">Jellemzők</label>
+            <select class="form-select me-1" id="iconOptions">
+                <option selected>Válassz...</option>
+                <?php
+                $sql = $this->prepare('SELECT * FROM '.$GLOBALS['prefix'].'features');
+                $sql->execute();
+                if($result = $sql->get_result()){
+                    if($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<option value="'.$row['featureIcon'].'">'.$row['featureName'].'</option>';
+                        }
                     }
                 }
-            }
-            ?>
-        </select>
-        <input type="number" class="form-control" placeholder="Csak számok!">
-        <span class="input-group-text">HUF.-/<i class="fa-regular fa-moon-stars"></i></span>
-    </div>
+                ?>
+            </select>
+            <input type="number" class="form-control" id="newRoomPrice" placeholder="Csak számok!">
+            <span class="input-group-text">HUF.-/<i class="fa-regular fa-moon-stars"></i></span>
+        </div>
 
-    <div class=" form-floating">
-        <textarea class="form-control" aria-label="With textarea" id="newroomdescription" maxlength="200" placeholder="Leírás, max. 200 karakter!" ></textarea>
-        <label id="newroomcount" for="description">Leírás, max 200 karakter!</label>
+        <div class=" form-floating">
+            <textarea class="form-control" aria-label="With textarea" id="newroomdescription" maxlength="200" placeholder="Leírás, max. 200 karakter!" ></textarea>
+            <label id="newroomcount" for="description">Leírás, max 200 karakter!</label>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="row">
+            <div class="card">
+                <img src="/korondi/Assets/images/defaultImage.png" class="card-img-top img-thumbnail">
+                <div class="card-body">
+                    <p class="card-text" id="prewDesc">Ez egy alapértelmezett leírás a szobáról. Valahogy így fog kinézni.</p>
+                </div>
+                <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item fs-3" id="prewFeatures"></li>
+                    <li class="list-group-item fs-5" id="prewPrice">.-HUF/<i class="fa-regular fa-moon-stars"></i></li>
+                    <li class="list-group-item fs-5" id="prewRoomNum"></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
